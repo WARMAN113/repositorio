ddbc485a3a4bba2303515e93787c3f2bdd862fcd
WARMAN113 <103250907+WARMAN113@users.noreply.github.com>
2022-05-05 12:58:33 +0200

    Add files via upload

---
+++ Actividad3.2.2/src/actividad3/pkg2/pkg2/Actividad322.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad3.pkg2.pkg2;
+
+/**
+ *
+ * @author mario
+ */
+public class Actividad322 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
